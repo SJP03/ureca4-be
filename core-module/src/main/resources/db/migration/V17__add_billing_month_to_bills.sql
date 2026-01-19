5046137e64ce289ccc4bf1630b40ff764506694f
shin-0328 <113588811+shin-0328@users.noreply.github.com>
2026-01-19 09:14:07 +0900

    Flyway V17,V18 add

--- core-module/src/main/resources/db/migration/V17__add_billing_month_to_bills.sql
+++ core-module/src/main/resources/db/migration/V17__add_billing_month_to_bills.sql
@@ -6,4 +6,6 @@
 
 ALTER TABLE BILLS
 ADD COLUMN billing_month CHAR(7) NOT NULL COMMENT 'YYYY-MM',
+ADD CONSTRAINT chk_billing_month_format
+CHECK (billing_month REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])$'),
 ADD UNIQUE KEY uk_user_month (user_id, billing_month);
